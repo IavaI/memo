b2547ed789d8da2d4ad5abc51c1ba6fc51c237c8
IavaI <3166896360@qq.com>
2024-10-03 10:36:49 +0800

    2024/10/3
    做到刚添加首页背景图片

--- app/src/main/java/com/example/contact/MainActivity.java
+++ app/src/main/java/com/example/contact/MainActivity.java
@@ -18,18 +18,18 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        //获取这个ID
-        Toolbar toolbar = this.findViewById(R.id.toolbar);
-        this.setSupportActionBar(toolbar);
+//        //获取这个ID
+//        Toolbar toolbar = this.findViewById(R.id.toolbar);
+//        this.setSupportActionBar(toolbar);
 
 
 
 
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
     }
 }
